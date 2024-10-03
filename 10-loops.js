f2b1e466aad9754beca9212c53178db028158fc1
Chancy Tausi Tsonga <122816618+Tausi95@users.noreply.github.com>
2024-10-04 00:23:37 +0200

    Delete 10-loops.js

--- 10-loops.js
+++
@@ -1,9 +0,0 @@
-// Using for...of loop to iterate through the array argument
-export default function appendToEachArrayValue(array, appendString) {
-  const resultArray = [];  // Create a new array for modified values
-  for (const value of array) {
-    resultArray.push(appendString + value);  // Append the string to each value
-  }
-  return resultArray;
-}
-
