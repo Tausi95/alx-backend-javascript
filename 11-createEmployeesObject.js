5c43a469a18b558340d105b62ee2aba1b750678f
Chancy Tausi Tsonga <122816618+Tausi95@users.noreply.github.com>
2024-10-04 00:23:49 +0200

    Delete 11-createEmployeesObject.js

--- 11-createEmployeesObject.js
+++
@@ -1,7 +0,0 @@
-// Function to create an object that contains a department and its employees
-export default function createEmployeesObject(departmentName, employees) {
-  return {
-    [departmentName]: [...employees],  // Using computed property names
-  };
-}
-
