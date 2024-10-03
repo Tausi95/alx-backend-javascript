20af2a39d953d7b147c3381f24f6bc72a52920d7
Chancy Tausi Tsonga <122816618+Tausi95@users.noreply.github.com>
2024-10-04 00:25:35 +0200

    Delete 8-getBudgetCurrentYear.js

--- 8-getBudgetCurrentYear.js
+++
@@ -1,9 +0,0 @@
-// Using ES6 syntax to define properties and methods in an object
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {
-    income,
-    gdp,
-    capita,
-  };
-}
-
