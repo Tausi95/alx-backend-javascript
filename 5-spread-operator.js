05be2b9d0a6c40d6f8ab947cdd6c566c91e8bded
Chancy Tausi Tsonga <122816618+Tausi95@users.noreply.github.com>
2024-10-04 00:25:00 +0200

    Delete 5-spread-operator.js

--- 5-spread-operator.js
+++
@@ -1,5 +0,0 @@
-// Using the spread operator to pass an array of values to a function
-export default function concatArrays(array1, array2, string) {
-  return [...array1, ...array2, ...string];  // Combine two arrays and a string
-}
-
