bdf3129a152b262bbbd2a31daf0fbe3dcf475ff9
Chancy Tausi Tsonga <122816618+Tausi95@users.noreply.github.com>
2024-10-04 00:24:37 +0200

    Delete 4-rest-parameter.js

--- 4-rest-parameter.js
+++
@@ -1,5 +0,0 @@
-// Using rest parameter syntax to gather all arguments into args array
-export default function returnHowManyArguments(...args) {
-  return args.length;  // Return the count of arguments
-}
-
