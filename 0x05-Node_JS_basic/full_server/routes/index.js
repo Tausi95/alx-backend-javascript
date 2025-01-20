8f5e93a94a0acd5c40fd430d33b32c927912169e
Mage107CT <effortbuildingcontractors@gmail.com>
2025-01-20 15:00:53 +0200

    added index.js file for the server

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,16 @@
+// This is routes/index.js
+
+
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+
+const router = express.Router();
+
+router.get('/', AppController.getHomepage);
+
+router.get('/students', StudentsController.getAllStudents);
+
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+module.exports = router;
