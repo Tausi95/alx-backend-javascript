671210a4c3dc15a4e8a20e25edc815a5e8928399
Mage107CT <effortbuildingcontractors@gmail.com>
2025-01-20 15:00:19 +0200

    added student and app controller files for the server

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,10 @@
+// This is AppController.js
+
+
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+module.exports = AppController;
